172d7575af5121e031ba809c5656b2cbfcbc9686
Nact <christiannakata9392@gmail.com>
2024-08-04 18:18:54 +0700

    main.py

--- main.py
+++ main.py
@@ -4,24 +4,19 @@ from nextcord.ext import commands
 from url_shortener import shorten_link
 
 
-# Create a bot instance
 intents = nextcord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Command to shorten URL
 @bot.command()
 async def shorten(ctx, link: str):
     result = shorten_link(link)
     await ctx.send(result)
 
-# Command to download yt vids
 bot.load_extension('ytdw')
 
-# Command to load class reminder
 bot.load_extension('class_reminder')
 
-# Run the bot
 @bot.event
 async def on_ready():
 	print(f"Logged in as: {bot.user.name}")
