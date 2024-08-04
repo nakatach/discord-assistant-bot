98597ec9c2fc628efe51551353b00943a24c0112
Nact <christiannakata9392@gmail.com>
2024-08-04 18:16:51 +0700

    ytdw.py

--- ytdw.py
+++ ytdw.py
@@ -12,10 +12,8 @@ class YTDownloader(commands.Cog):
     async def download_to_laptop(self, ctx, url: str):
         await ctx.send(f"Fetching video information for {url}")
         try:
-            # Define the path to cookies file (if needed)
             cookies_file = 'www.youtube.com_cookies.txt'
 
-            # Create yt_dlp options to get information about the video
             ydl_opts = {
                 'format': 'bestaudio/best',  # Get the best available audio format
                 'noplaylist': True,
@@ -27,13 +25,11 @@ class YTDownloader(commands.Cog):
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                 info = ydl.extract_info(url, download=False)
 
-            # List available resolutions
             formats = info.get('formats', [])
             resolutions = [f['height'] for f in formats if 'height' in f and f['height'] is not None]
             resolutions = list(set(resolutions))  # Remove duplicates
-            resolutions.sort(reverse=True)  # Sort resolutions in descending order
+            resolutions.sort(reverse=True)
 
-            # Display available resolutions
             resolution_message = "Available resolutions:\n"
             for res in resolutions:
                 resolution_message += f"{res}p\n"
@@ -51,18 +47,16 @@ class YTDownloader(commands.Cog):
                 await ctx.send("Invalid resolution selected. Downloading the highest resolution available.")
                 selected_resolution = resolutions[0]
 
-            # Define download path for laptop
             download_folder = str('C:/Users/MyBook Hype AMD/Downloads')
             await ctx.send(f"Saving video to {download_folder}")
 
-            # Download the video
             ydl_opts = {
                 'format': f'best[height<={selected_resolution}]',
                 'outtmpl': os.path.join(download_folder, '%(title)s.%(ext)s'),
                 'noplaylist': True,
-                'cookiefile': cookies_file,  # Specify the path to cookies
+                'cookiefile': cookies_file,
                 'progress_hooks': [self.progress_hook],
-                'merge_output_format': None,  # Avoid merging formats
+                'merge_output_format': None,
             }
 
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
